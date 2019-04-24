94c83b14ed3235e08ff235816af97732c76643b0
cmcca <cata92.mh@gmail.com>
2019-04-23 21:24:29 -0400

    basics

---
+++ client/src/components/Settings.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Settings(){
+    return(
+        <h1> SETTINGS </h1>
+    )
+}
+
+export default Settings;
---
+++ client/src/components/Splash.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import alarming from './alarming.png'
+
+function Splash(){
+    return(
+        <img src={alarming} alt="Alarming" id= "logo" />
+    )
+}
+
+export default Splash;
