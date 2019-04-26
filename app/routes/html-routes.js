195ae665a65c1361324eb903d3a3f3e8b327f4d4
cmcca <cata92.mh@gmail.com>
2019-04-25 21:04:32 -0400

    style things

--- app/routes/html-routes.js
+++ app/routes/html-routes.js
@@ -1,19 +1,21 @@
 var path = require("path");
-
-
+var express = require("express");
+var app = express();
+​
+​
 // Routes
 // =============================================================
 module.exports = function(app) {
-
-  // Each of the below routes just handles the HTML page that the user gets sent to.
-
-  // index route loads view.html
-  app.get("/", function(req, res) {
-    res.render("index");
-  });
-
-
-
-
-
+​
+ // Each of the below routes just handles the HTML page that the user gets sent to.
+​
+ // index route loads view.html
+ app.get("/", function(req, res) {
+  res.render("client/public/index");
+ });
+​
+​
+​
+​
+​
 };
