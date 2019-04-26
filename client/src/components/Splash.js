59cd2566bf7cc487afad7ce2c2e73693dbdb3493
cmcca <cata92.mh@gmail.com>
2019-04-25 21:03:35 -0400

    playing with styles

--- client/src/components/Splash.js
+++ client/src/components/Splash.js
@@ -1,9 +1,31 @@
 import React from 'react';
-import alarming from './alarming.png'
+import { Link } from "react-router-dom";
+import alarming from './alarming.png';
+
 
 function Splash(){
     return(
+        <div>
         <img src={alarming} alt="Alarming" id= "logo" />
+        <ul id="links">
+        <li>
+          <Link
+            to="/alarm"
+            className={window.location.pathname === "/about" ? "nav-link active" : "nav-link"}
+          >
+            Alarm
+          </Link>
+        </li>
+        <li>
+          <Link
+            to="/settings"
+            className={window.location.pathname === "/discover" ? "nav-link active" : "nav-link"}
+          >
+            Settings
+          </Link>
+        </li>
+        </ul>
+        </div>
     )
 }
 
